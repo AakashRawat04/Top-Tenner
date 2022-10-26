4902274c21e54902dfe19077be7b4beea5da6467
Ash <01tripster@gmail.com>
2022-10-26 07:08:36 +0530

    getAllResults integration and added search button

--- app.js
+++ app.js
@@ -1,16 +1,68 @@
+const fetch = require("node-fetch");
+
 const options = {
-	method: "POST",
-	url: "google-search-5.p.rapidapi.com",
+	method: "GET",
 	headers: {
-		"content-type": "application/json",
-		"X-RapidAPI-Key": `ea6c510851msh7d4d310eddd33b1p1ffccajsnbc14787d50ae`,
-		"X-RapidAPI-Host": "google-search-5.p.rapidapi.com",
+		"X-RapidAPI-Key": "ea6c510851msh7d4d310eddd33b1p1ffccajsnbc14787d50ae",
+		"X-RapidAPI-Host": "google-search-2.p.rapidapi.com",
 	},
-	data: '{"query":"hello world","gl":"US","hl":"en_US","device":"desktop","duration":"","autocorrect":1,"page":1,"uule":"this is secret","pages":2}',
 };
 
-async function getAllResults() {
+// fetch(url, options)
+// 	.then(res => res.json())
+// 	.then(json => console.log(json))
+// 	.catch(err => console.error('error:' + err));
+
+async function getAllResults(query) {
+	const url = `https://google-search-2.p.rapidapi.com/organicResults?query=${query}&num=10&gl=us&hl=en&page=0&nfpr=0&device=desktop`;
 	console.log("get all results page.");
+	fetch(url, options)
+		.then((res) => res.json())
+		.then((json) => {
+			for (i = 0; i < json.organic_results.length; i++) {
+				const heading = json.organic_results[i].title;
+
+				//all results section
+				const results_section =
+					document.getElementsByClassName("results-section");
+
+				//main div result_element --> everything inside result_element
+				const result_element = document.createElement("div");
+				result_element.classList.add(
+					"mt-10 hover:shadow-xl border-2 rounded-md border-transparent p-5 bg-purple-700 transition ease-in-out delay-75 hover:-translate-y-0 hover:scale-110 duration-200 hover:shadow-cyan-500/20"
+				);
+
+				//heading of the result element
+				const result_heading = document.createElement("span");
+				result_heading.classList.add(
+					"heading text-xl font-bold hover:underline hover:text-blue-400 hover:cursor-pointer"
+				);
+
+				//a tag for heading
+				const heading_link = document.getElementById("heading");
+				heading_link.setAttribute("href", `${json.organic_results[i].link}`);
+
+				//injecting anchor tag in span
+				result_heading.appendChild(heading_link);
+
+				//injecting final heading in result element
+				result_element.appendChild(result_heading);
+
+				//adding hr break line
+				const hr = document.createElement("hr");
+				hr.classList.add("border border-cyan-400 my-2");
+				result_element.appendChild(hr);
+
+				//discruption
+				const discruption = document.createElement("div");
+				discruption.classList.add("p-1 text-black");
+				result_element.appendChild(discruption);
+
+				//adding the final div to the main div
+				results_section.appendChild(result_element);
+			}
+		})
+		.catch((err) => console.error("error:" + err));
 }
 
 async function getNews() {
